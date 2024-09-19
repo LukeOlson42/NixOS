9d22e83ef2e637b36e10ffc8af09020b2b455186
Luke Olson <olsonluke42@gmail.com>
2024-09-19 19:22:21 -0400

    fixing theme again again again

--- modules/cli.nix
+++ modules/cli.nix
@@ -3,7 +3,7 @@
 let
     zshTheme = ''
         autoload -U colors && colors
-        PROMPT='%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b '
+        PROMPT='%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$fg[purple]%} $%b '
     '';
     prependZshCustom = ''
         export ZSH_CUSTOM="${config.home.homeDirectory}/.oh-my-zsh/custom"
