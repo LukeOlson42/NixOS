e5b0d523c711613a4ff93371f7aec8d8ca071293
Luke Olson <olsonluke42@gmail.com>
2024-09-19 17:31:37 -0400

    hopefully fixed borked hyprland

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,28 +1,32 @@
 { config, pkgs, ... }:
 
 {
-#    home = {
-#		packages = with pkgs; [
-#            waybar
-#            mako
-#            libnotify
-#            swww
-#            rofi-wayland
-#		];
-#    };
+    home = {
+		packages = with pkgs; [
+            waybar
+            mako
+            libnotify
+            swww
+            rofi-wayland
+		];
+    };
 
-    # programs.kitty.enable = true;
+    programs.kitty.enable = true;
     wayland.windowManager.hyprland = {
         enable = true;
         settings = {
-            "$mod" = "SUPER";
+            "$mainMod" = "SUPER";
+            "$terminal" = "alacritty";
+            "$browser" = "firefox";
+            "$editor" = "nvim";
+            "$fileManager" = "yazi";
             bind = [
-                "$mod, F, exec, firefox"
+                "$mainMod, f, exec, firefox"
             ] ++ (
                 builtins.concatLists (builtins.genList (i:
                     let ws = i + 1;
                     in [
-                        "$mod, code:1${toString i}, workspace, ${toString ws}"
+                        "$mainMod, code:1${toString i}, workspace, ${toString ws}"
                     ]
                 ) 9)
             );
